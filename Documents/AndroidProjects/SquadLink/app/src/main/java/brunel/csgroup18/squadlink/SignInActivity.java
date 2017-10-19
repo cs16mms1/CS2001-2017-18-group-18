95b3f45c2c17fdc5adb0a221437c8b2772577676
Nathan Hoy <nathan_hoy@live.co.uk>
2017-10-19 09:54:13 +0100

    Just testing GitHub

--- Documents/AndroidProjects/SquadLink/app/src/main/java/brunel/csgroup18/squadlink/SignInActivity.java
+++ Documents/AndroidProjects/SquadLink/app/src/main/java/brunel/csgroup18/squadlink/SignInActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 
 public class SignInActivity extends AppCompatActivity {
 
+    private int test;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
     }
 }
