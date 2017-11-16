c2531592eab0dee1d0c481bc0f4cf414370ed2c9
Nathan Hoy <nathan_hoy@live.co.uk>
2017-11-16 17:33:10 +0000

    Got nav drawer working. Currently only starts overview fragement.

--- SquadLink/app/src/main/java/brunel/csgroup18/squadlink/MainActivity.java
+++ SquadLink/app/src/main/java/brunel/csgroup18/squadlink/MainActivity.java
@@ -1,13 +1,72 @@
 package brunel.csgroup18.squadlink;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.nav_action_bar);
+        setSupportActionBar(mToolbar);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.closed);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+        switch (item.getItemId()){
+
+            case(R.id.nav_overview):{
+                OverviewFragment overviewFragment = new OverviewFragment();
+                ft.replace(R.id.fragment_container, overviewFragment);
+                ft.commit();
+                mToolbar.setTitle("Overview");
+            }
+            break;
+        }
+
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
