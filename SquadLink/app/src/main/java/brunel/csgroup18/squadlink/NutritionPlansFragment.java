fa2e710ed4fde6fe4a900bcdbb5629893e24098d
Nathan Hoy <nathan_hoy@live.co.uk>
2017-11-18 01:02:45 +0000

    Implemented Tabs to Nutrition Plans, colours may need adjustment

--- SquadLink/app/src/main/java/brunel/csgroup18/squadlink/NutritionPlansFragment.java
+++ SquadLink/app/src/main/java/brunel/csgroup18/squadlink/NutritionPlansFragment.java
@@ -3,9 +3,11 @@ package brunel.csgroup18.squadlink;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTabHost;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TabHost;
 
 
 /**
@@ -13,6 +15,7 @@ import android.view.ViewGroup;
  */
 public class NutritionPlansFragment extends Fragment {
 
+    private FragmentTabHost mTabHost;
 
     public NutritionPlansFragment() {
         // Required empty public constructor
@@ -22,8 +25,18 @@ public class NutritionPlansFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nutrition_plans, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_nutrition_plans, container, false);
+
+
+        mTabHost = (FragmentTabHost) rootView.findViewById(android.R.id.tabhost);
+        mTabHost.setup(getActivity(), getChildFragmentManager(), R.id.realtabcontent);
+
+        mTabHost.addTab(mTabHost.newTabSpec("Coach Plans").setIndicator("Coach Plans"),
+                CoachNutritionFragment.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("My Plans").setIndicator("My Plans"),
+                MyNutritionFragment.class, null);
+
+        return rootView;
     }
 
 }
