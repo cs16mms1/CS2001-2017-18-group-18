1e18756d2b2855eff18c129b57d1708b4b105d13
yasvanth1 <yasvanth1@hotmail.co.uk>
2017-11-20 11:14:41 +0000

    Experimenting with my database

--- SquadLink/app/src/main/java/brunel/csgroup18/squadlink/SignInActivity.java
+++ SquadLink/app/src/main/java/brunel/csgroup18/squadlink/SignInActivity.java
@@ -1,13 +1,43 @@
 package brunel.csgroup18.squadlink;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignInActivity extends AppCompatActivity {
-
+    private Button nAthlete,nTrainer; //calling the button
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        nTrainer = (Button)findViewById(R.id.trainer);
+        nAthlete = (Button)findViewById(R.id.athlete);
+
+        nTrainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(SignInActivity.this,TrainerLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+        nAthlete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(SignInActivity.this,AthleteLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+
     }
 }
