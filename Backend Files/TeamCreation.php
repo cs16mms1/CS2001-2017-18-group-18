98641f7d77d43900ef79209230c5ac14c97a3eb7
Nathan Hoy <nathan_hoy@live.co.uk>
2018-01-16 19:47:57 +0000

    Posting team credentials correctly to table in DB

--- Backend Files/TeamCreation.php
+++ Backend Files/TeamCreation.php
@@ -8,6 +8,14 @@ $postcode = $_POST["postcode"];
 $email = $_POST["email"];
 $phone = $_POST["phone"];
 
-var_dump($teamname,$postcode,$email,$phone);
+  insertTeam();
+
+      function insertTeam(){
+        global $connect, $teamname, $postcode, $email, $phone;
+
+        var_dump($teamname,$postcode,$email,$phone);
+        $query = mysqli_query($connect,"INSERT INTO team (teamname, postcode, email, phone)  VALUES '$teamname','$postcode','$email','$phone'");
+
+      }
 
   ?>
