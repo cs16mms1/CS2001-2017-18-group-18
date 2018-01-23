5a4f6244ca3eb638dbb27a25fb7a629c636622e0
Nathan Hoy <nathan_hoy@live.co.uk>
2018-01-23 14:03:18 +0000

    Returning team id from newly added teams so the link table can be filled out

--- Backend Files/TeamCreation.php
+++ Backend Files/TeamCreation.php
@@ -7,15 +7,25 @@ $teamname = $_POST["teamname"];
 $postcode = $_POST["postcode"];
 $email = $_POST["email"];
 $phone = $_POST["phone"];
+$id = $_SESSION['id'];
+
 
   insertTeam();
-
+  getTeamId();
       function insertTeam(){
         global $connect, $teamname, $postcode, $email, $phone;
-
-        var_dump($teamname,$postcode,$email,$phone);
-        $query = mysqli_query($connect,"INSERT INTO team (teamname, postcode, email, phone)  VALUES '$teamname','$postcode','$email','$phone'");
+              var_dump($connect,$teamname,$postcode,$email,$phone);
+        $query = mysqli_query($connect,"INSERT INTO team (teamname, postcode, email, phone)  VALUES ('$teamname','$postcode','$email','$phone')");
 
       }
+
+      function getTeamId(){
+        global $connect, $teamname;
+        $result = mysqli_query($connect, "SELECT team_id FROM team WHERE teamname = '$teamname' ");
+        $row = mysqli_fetch_assoc($result);
+  			$teamid = (int) $row['team_id'];
+        var_dump($teamid);
+      }
+
 
   ?>
