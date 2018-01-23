d6a3c31ec226e5048fb522107ff910863948b361
Nathan Hoy <nathan_hoy@live.co.uk>
2018-01-23 22:22:51 +0000

    Added map to fragment, will now adjust depending on location data

--- SquadLink/app/src/main/java/brunel/csgroup18/squadlink/TeamFinderFragment.java
+++ SquadLink/app/src/main/java/brunel/csgroup18/squadlink/TeamFinderFragment.java
@@ -2,17 +2,25 @@ package brunel.csgroup18.squadlink;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class TeamFinderFragment extends Fragment {
+public class TeamFinderFragment extends Fragment implements OnMapReadyCallback{
 
+    GoogleMap map;
+    MapView mapView;
+    View mView;
 
     public TeamFinderFragment() {
         // Required empty public constructor
@@ -23,7 +31,26 @@ public class TeamFinderFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_team_finder, container, false);
+        mView = inflater.inflate(R.layout.fragment_team_finder, container, false);
+
+        return mView;
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mapView = (MapView) mView.findViewById(R.id.map);
+        mapView.onCreate(null);
+        mapView.onResume();
+        mapView.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        
+    }
 }
