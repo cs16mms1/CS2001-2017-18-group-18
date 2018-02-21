88e4d54641fd6459071d07196d8c6f37abfa3e44
Nathan Hoy <nathan_hoy@live.co.uk>
2018-02-21 16:07:28 +0000

    querying the database for relevent associated teams

--- Website/MyTeams.php
+++ Website/MyTeams.php
@@ -1,7 +1,31 @@
 <?php
+
+	include 'dbconnect.php';
 	session_start();
 	echo("test");
 	echo($_SESSION['id']);
+
+	$user_id = $_SESSION['id'];
+
+	returnTeams();
+
+	function returnTeams(){
+		global $connect, $user_id;
+		$query = "SELECT * FROM `team` INNER JOIN `users_has_team` ON
+							users_has_team.team_team_id = team.team_id WHERE
+							users_has_team.users_user_id = '$user_id'";
+		$result = mysqli_query($connect, $query);
+		$row = mysqli_fetch_assoc($result);
+
+		echo($row['teamname']);
+		echo ($row['team_id']);
+
+		$_SESSION['teamname'] = $row['teamname'];
+		$_SESSION['team_id'] = $row['team_id'];
+
+	}
+
+
 ?>
 
 <!DOCTYPE html>
