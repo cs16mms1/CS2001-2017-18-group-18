1054b353b44b126df0ab1d6629dca1a083c750cc
Nathan Hoy <nathan_hoy@live.co.uk>
2018-02-22 14:45:05 +0000

    List of teams now link to pages where training plans can be sent. The links carry the player's username/id so queries can be performed on the db. After posting these training plans, we will work on getting Android to check the DB for them

---
+++ Website/ManagePlayer.php
@@ -0,0 +1,19 @@
+
+<?php
+  include 'dbconnect.php';
+
+
+  echo($_POST['username']);
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
--- Website/ManageTeam.php
+++ Website/ManageTeam.php
@@ -8,20 +8,18 @@ echo($_SESSION['team_id']);
 
 $team_id = $_SESSION['team_id'];
 
-returnTeamMembers();
-
 function returnTeamMembers(){
   global $connect, $user, $team_id;
 
   $query = "SELECT * FROM `users` INNER JOIN `users_has_team` ON
             users_has_team.users_user_id = users.user_id WHERE
-            users_has_team.team_team_id = '$team_id'";
+            users_has_team.team_team_id = '$team_id' AND users.type LIKE 'Player'";
 
             $result = mysqli_query($connect, $query);
 
             while ($row = mysqli_fetch_assoc($result)) {
             $username = $row['username'];
-       echo ("<br/><a href=\"CreateTeam.php\">$username</a>");
+       echo ("<br/><input type=\"submit\" name=\"username\" value=$username>");
    }
 }
 
@@ -34,6 +32,10 @@ function returnTeamMembers(){
     <title></title>
   </head>
   <body>
+
+      <form action="ManagePlayer.php" method="post">
+          <?php returnTeamMembers() ?>
+      </form>
 
   </body>
 </html>
