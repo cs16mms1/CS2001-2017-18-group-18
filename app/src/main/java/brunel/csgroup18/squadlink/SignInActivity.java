e6be7fc82eba1ac5be186baf496559dc73a45f65
1605676 <tanvir_ali@hotmail.co.uk>
2018-04-21 18:25:41 +0100

    Add google play json

--- app/src/main/java/brunel/csgroup18/squadlink/SignInActivity.java
+++ app/src/main/java/brunel/csgroup18/squadlink/SignInActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.Map;
 
 public class SignInActivity extends AppCompatActivity {
@@ -12,7 +15,11 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-        Intent intent = new Intent(SignInActivity.this, MapsActivity.class);
-        startActivity(intent);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 }
