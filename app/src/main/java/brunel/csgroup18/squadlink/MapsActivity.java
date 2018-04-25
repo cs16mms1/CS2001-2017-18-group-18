afafc33b82442dbace3fbcaed160d069525a8109
yasvanth1 <yasvanth1@hotmail.co.uk>
2018-04-25 18:32:55 +0100

    Fixed Google Maps Backend

--- app/src/main/java/brunel/csgroup18/squadlink/MapsActivity.java
+++ app/src/main/java/brunel/csgroup18/squadlink/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        LatLng sydney = new LatLng(51.52, -0.47);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Brunel University Event"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
